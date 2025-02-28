4cdc4671e61d1e2983d963ff9f83082c41e0e4d0
alfaromartino <alfaromartino@gmail.com>
2025-02-28 14:44:56 -0700

    update

--- src/FastBenchmark.jl
+++ src/FastBenchmark.jl
@@ -3,29 +3,36 @@ module FastBenchmark
 using Chairmarks
 
 function mybenchmark_implementation(expr)
-    quote
-        object = Chairmarks.@b $(expr)
+    # Capture the Chairmarks module in a QuoteNode so that it's available
+    # in the generated code independently of the caller's scope.
+    local cm = QuoteNode(Chairmarks)
+    return quote
+        # Bind the captured module to a local variable.
+        local _cm = cm
+        # Use the Chairmarks macro via the captured module reference.
+        object = _cm.@b (expr)
         result = sprint(show, "text/plain", object)
-        
+
         if object.allocs == 0
             result *= " (0 allocations: 0 bytes)"
         else
             result = replace(result, "allocs" => "allocations")
         end
-        
+
         # Remove any other information that comes after the allocations if it exists        
         #result = replace(result, r",(?![^,]*\bgc\b).*$" => "")     # to keep gc time
-        result  = replace(result, r",.*$" => ")")                   # to get rid of everything
-        result  = replace(result, "(without a warmup) " => "")
+        result = replace(result, r",.*$" => ")")                   # to get rid of everything
+        result = replace(result, "(without a warmup) " => "")
 
         println("  " * result)  # Add two spaces before the result
     end
+
 end
 
 macro ctime(expr)
-    return esc(mybenchmark_implementation(expr))
+    # We return the quoted code as-is since it already captures Chairmarks.
+    return mybenchmark_implementation(expr)
 end
 
 export @ctime
-
-end
+end # module FastBenchmark
